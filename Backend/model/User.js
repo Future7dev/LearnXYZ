accb99a6bdc84c0a0a3cd605f4d0c8a1cb242efa
Future7dev <koleypriyam8@gmail.com>
2026-09-01 00:51:54 +0530

    user model update

--- Backend/model/User.js
+++ Backend/model/User.js
@@ -2,28 +2,30 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema({
-    name: String,
+    name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    password: {
+        type: String,
+        required: function () {
+            return !this.googleId;
+        },
+    },
+    googleId: { type: String, default: null },
+    avatar: { type: String, default: null },
+    authProvider: { type: String, enum: ["local", "google"], default: "local" },
+    isAdmin: { type: Boolean, default: false },
+}, { timestamps: true });
 
-});
 userSchema.pre("save", async function () {
-    if (!this.isModified("password")) return;
+    if (!this.password || !this.isModified("password")) return;
 
     const salt = await bcrypt.genSalt(10);
-
-    this.password = await bcrypt.hash(
-        this.password,
-        salt
-    );
+    this.password = await bcrypt.hash(this.password, salt);
 });
-userSchema.methods.comparePassword = async function (
-    enteredPassword
-) {
-    return await bcrypt.compare(
-        enteredPassword,
-        this.password
-    );
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+    if (!this.password) return false;
+    return await bcrypt.compare(enteredPassword, this.password);
 };
 
 const User = mongoose.model("User", userSchema);
