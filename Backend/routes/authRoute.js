248894489c87cada3032adfebc9f4c83aa5eb70f
Future7dev <koleypriyam8@gmail.com>
2026-09-01 00:51:38 +0530

    auth route update

--- Backend/routes/authRoute.js
+++ Backend/routes/authRoute.js
@@ -1,10 +1,13 @@
 import express from "express";
-import { login, register, getProfile, updateProfile } from "../controllers/authController.js";
+import { login, register, googleAuth, getProfile, updateProfile } from "../controllers/authController.js";
+import { protect } from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 
 router.post("/login", login);
 router.post("/register", register);
-router.get("/profile", getProfile);
-router.put("/profile", updateProfile);
+router.post("/google", googleAuth);
+router.get("/profile", protect, getProfile);
+router.put("/profile", protect, updateProfile);
 
 export default router;
